f928250b385e5041436491bb586a853710db7ce1
komal837-gif <utagekomal57@gmail.com>
2026-01-03 21:25:13 +0530

    initial files created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MobileCardComponent } from './shared/components/mobile-card/mobile-card.component';
+import { MobileDashboardComponent } from './shared/components/mobile-dashboard/mobile-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileCardComponent,
+    MobileDashboardComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
